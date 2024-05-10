898335bf84fd50ac575549b027852b86e090d903
Justin Kahn <justin@Justins-MacBook-Air.local>
2024-05-10 11:13:48 -0500

    retrieval of numbers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,23 @@
 console.log('client.js is sourced!');
+
+
+
+
+
+    function equalsClick(event) {
+        event.preventDefault(); 
+    
+        
+        const numberOne = document.getElementById('numberOne').value;
+        const numberTwo = document.getElementById('numberTwo').value;
+    
+        console.log('First Number:', numberOne);
+        console.log('Second Number:', numberTwo);
+    
+    
+    }
+
+
+
+
+
