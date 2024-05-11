569fc4c3eb9da65d524aab723b63fa8b3b970c32
Justin Kahn <justin@Justins-MacBook-Air.local>
2024-05-11 13:47:15 -0500

    passing in numbers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,27 @@
 console.log('client.js is sourced!');
 
 
+function  calculateNumbers(a,b) {
+    console.log("calculateNumbers() is working!")
+    console.log("Succesfully passing in the first number", a, "And the second number:", b)
+
+}
+
+function add(a,b) {
+return a+b
+}
+function subtract(a,b){
+return a-b
+}
+
+function multiply(a,b){
+return a*b;
+}
+
+function divide(a,b){
+return a/b
+
+}
 
 
 
@@ -10,12 +31,17 @@ console.log('client.js is sourced!');
         
         const numberOne = document.getElementById('numberOne').value;
         const numberTwo = document.getElementById('numberTwo').value;
-    
         console.log('First Number:', numberOne);
         console.log('Second Number:', numberTwo);
+
+        calculateNumbers(numberOne, numberTwo)
+
     
     
     }
+
+
+    
 
 
 
