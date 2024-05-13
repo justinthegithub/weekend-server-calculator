0a6946ce8384c313e21314768a6ccb4b14e2bbfa
Justin Kahn <justin@Justins-MacBook-Air.local>
2024-05-13 11:10:01 -0500

    updated following live code and now just 6 tests are passing

--- server/server.js
+++ server/server.js
@@ -1,147 +1,42 @@
 const express = require('express');
 const app = express();
-let PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.static('server/public'));
 
-// Global variable that will contain all of the
-// calculation objects:
-
-let calculations = []
-
-
-// Here's a wonderful place to make some routes:
-
-// GET /calculations
-
-
-//example datafrom instructions.md
- /*calculations = [
-  {
-    numOne: 3,
-    numTwo: 5,
-    operator: '+',
-    result: 8
-  },
-  {
-    numOne: 11,
-    numTwo: 7,
-    operator: '-',
-    result: 4
-  }
-];
-*/
-calcHistory=[  {
-  numOne: 11,
-  numTwo: 7,
-  operator: '-',
-  result: 4
-}]
-
-app.get(('/getHistory'), (req, res)=>{
-res.send(calcHistory)
-})
-
-
-
+let calculations = [];
 
 app.get('/calculations', (req, res) => {
-  console.log('app.get /calculations is working in server.js')
-  console.log(`the request: ${req} and the response:${res}`)
-  res.status(200).json(calculations);
-  console.log(req.data)
+    res.status(200).json(calculations);
 });
 
-// POST /calculations
+app.post('/calculations', (req, res) => {
+    const { numOne, numTwo, operator } = req.body;
+    const result = decideCalculation(numOne, numTwo, operator);
+    const newCalculation = { numOne, numTwo, operator, result };
+    calculations.push(newCalculation);
+    res.status(201).json(newCalculation);
+});
 
-function  calculateNumbers(a,b) {
-  console.log("calculateNumbers() is working!")
-  console.log("Succesfully passing in the first number", a, "And the second number:", b)
-
+function decideCalculation(numOne, numTwo, operator) {
+    numOne = Number(numOne);
+    numTwo = Number(numTwo);
+    switch (operator) {
+        case '+': return numOne + numTwo;
+        case '-': return numOne - numTwo;
+        case '*': return numOne * numTwo;
+        case '/': return numOne / numTwo;
+        default: return NaN;
+    }
 }
 
-function add(a,b) {
-return Number(a)+Number(b)
-}
-function subtract(a,b){
-return a-b
-}
+const server = app.listen(PORT, () => console.log(`Server running on: ${PORT}`));
 
-function multiply(a,b){
-return a*b;
-}
-
-function divide(a,b){
-return a/b
-
-}
-
-function decideCalculation(a, b, operator){
-  a=Number(a)
-  b=Number(b)
-if (operator ==='+'){
-return add(a,b)
-}
-else if (operator ==='-'){
-  return subtract(a,b)
-}
-else if (operator ==='*'){
-  return multiply(a,b)
-}
-else if (operator ==='/'){
-  return divide(a,b)
-}
-}
-
-
-
-
-app.post('/calculations', (req, res)=>{
-  console.log("app.post /calculuations is working. Here is the req", req)
-  let {numOne,numTwo,operator}=req.body
-let result =decideCalculation(numOne, numTwo, operator);
-let newCalculation={numOne,numTwo,operator, result};
-calculations.push(newCalculation);
-res.status(201).json(newCalculation)
-
-
-
-
-})
-
-
-
-
-
-
-
-// PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
-// 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
-
-// Makes it so you don't have to kill the server
-// on 5000 in order to run the tests:
+// For testing purposes:
 if (process.env.NODE_ENV === 'test') {
-  PORT = 5001;
-}
-
-// This starts the server...but also stores it in a variable.
-// This is weird. We have to do it for testing reasons. There
-// is absolutely no need for you to reason about this.
-const server = app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
-});
-
-// server.setTimeout(500)
-
-// This is more weird "for testing reasons" code. There is
-// absolutely no need for you to reason about this.
-app.closeServer = () => {
-  server.close();
-}
-
-app.setCalculations = (calculationsToSet) => {
-  calculations = calculationsToSet;
+    app.setCalculations = (calculationsToSet) => { calculations = calculationsToSet; };
+    app.closeServer = () => { server.close(); };
 }
 
 module.exports = app;
